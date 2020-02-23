96562582d10fdb103dc8a57c44ddfb7222eee40f
César Augusto Santos de Almeida <ocesaralmeida@gmail.com>
2020-02-23 16:32:04 -0300

    finalizada aula sobre criar um projeto dart

---
+++ 7183-logica-de-programacao-com-dart/estudo/lib/main.dart
@@ -0,0 +1,8 @@
+
+void main(List<String> arguments) {
+  if(arguments[0] == "hackear") {
+    print("hackeamento concluído");
+  } else if(arguments[0] == "imprimir") {
+    print("executando impressão");
+  }
+}
