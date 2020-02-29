ed3ffac70435a28a3381e825e6b382689eb2bd56
César Augusto Santos de Almeida <ocesaralmeida@gmail.com>
2020-02-29 11:04:10 -0300

    concluida aula de adicionar itens na lista

--- 7184-criando-primeiro-app-flutter/todo/lib/main.dart
+++ 7184-criando-primeiro-app-flutter/todo/lib/main.dart
@@ -33,12 +33,38 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  var newTaskCtrl = TextEditingController();
+
+  void add() {
+    setState(() {
+      widget.items.add(
+        Item(
+          title: newTaskCtrl.text,
+          done: false,
+        ),
+      );
+      newTaskCtrl.text = "";
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Todo List"),
-      ),
+          title: TextFormField(
+        controller: newTaskCtrl,
+        keyboardType: TextInputType.text,
+        style: TextStyle(
+          color: Colors.white,
+          fontSize: 18,
+        ),
+        decoration: InputDecoration(
+          labelText: "Nova Tarefa",
+          labelStyle: TextStyle(
+            color: Colors.white,
+          ),
+        ),
+      )),
       body: ListView.builder(
         itemCount: widget.items.length,
         itemBuilder: (BuildContext ctxt, int index) {
@@ -55,6 +81,11 @@ class _HomePageState extends State<HomePage> {
             },
           );
         },
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: add,
+        child: Icon(Icons.add),
+        backgroundColor: Colors.pink,
       ),
     );
   }
